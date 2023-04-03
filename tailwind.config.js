996e51cc595e9b8079261e7abab9b06c64e6e877
Nico Limo <nicolas.limonoff@gmail.com>
2023-04-03 11:35:25 -0300

    added font and setted into tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dmsans: ['DM Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
